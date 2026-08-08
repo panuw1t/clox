13e3b7690b1b2450754187be1eaff8578a7f5702
panuvit sreudomdetsakul <panuvit@asger.co>
2026-08-08 07:19:39 +0700

    add local variable

--- src/debug.c
+++ src/debug.c
@@ -45,6 +45,12 @@ static int simpleInstruction(const char* name, int offset) {
   return offset + 1;
 }
 
+static int byteInstruction(const char* name, Chunk* chunk, int offset) {
+  uint8_t slot = chunk->code[offset + 1];
+  printf("%-16s %4d\n", name, slot);
+  return offset + 2;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
   printf("%04d ", offset);
   int line = getLine(&chunk->lines, offset);
@@ -69,6 +75,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     return simpleInstruction("OP_FALSE", offset);
   case OP_POP:
     return simpleInstruction("OP_POP", offset);
+  case OP_GET_LOCAL:
+    return byteInstruction("OP_GET_LOCAL", chunk, offset);
+  case OP_SET_LOCAL:
+    return byteInstruction("OP_SET_LOCAL", chunk, offset);
   case OP_GET_GLOBAL:
     return variableInstruction("OP_GET_GLOBAL", chunk, offset);
   case OP_DEFINE_GLOBAL:
